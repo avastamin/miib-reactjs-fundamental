ae92e5ff4ba63f832e7c9e54e91c3ec58cb29329
Ruhul Amin <ruhulamin@Ruhuls-MBP-2.fritz.box>
2018-03-18 00:53:51 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Reactjs বেসিক কোর্স
+
+এই কোর্স Reactjs এর বেসিক কোর্স। নিচে দেখুন কিভাবে এই অ্যাপটি ব্যবহার করতে হবে ।
+
+
+## How to use:
+    1. Clone or download the files
+    2. Go to that folder
+    3. Use npm install to install all dependencies
+    4. use npm start then you will see the project on http://localhost:3000/
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <div className="HeaderWrap">
+        <header className="App-header">
+          <h1 className="App-title">Reactjs অ্যাপ্লিকেশান এ স্বাগতম</h1>
+        </header>
+        </div>
+      </div>
+    );
+  }
+}
+export default App;
